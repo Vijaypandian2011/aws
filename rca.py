b020e7fac485c3ad6f4a89acbcbdb910d99330f7
Vijay Pandian <120645976+Vijaypandian2011@users.noreply.github.com>
2024-03-07 13:16:43 +0530

    Update rca.py

--- rca.py
+++ rca.py
@@ -1,21 +1,35 @@
-# pip install streamlit langchain lanchain-openai beautifulsoup4 python-dotenv chromadb
-#new croma
+# pip install streamlit 
+# pip install langchain 
+# pip install openai 
+# pip install beautifulsoup4 
+# pip install python-dotenv 
+# pip install chromadb
+# pip install langchain-openai==0.0.8
+# pip install langchain-community
+
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-# from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain, ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chains.combine_documents import create_stuff_documents_chain
 import os
- 
-os.environ['OPENAI_API_KEY'] = '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'
- 
-# load_dotenv()
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+# Function to get the OpenAI API key from the user through a modal
+def no_ingest_docs():
+    persist_directory = './vector_data'
+    embeddings = OpenAIEmbeddings()
+    vector_store = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+    return vector_store
+    
+
+
  
 def get_vectorstore_from_url(url):
     # get the text in document form
@@ -27,21 +41,16 @@ def get_vectorstore_from_url(url):
     document_chunks = text_splitter.split_documents(document)
  
     embeddings = OpenAIEmbeddings()
-    vector_store=Chroma.from_documents(document_chunks, embedding=embeddings,persist_directory='./vector_data_openAI')
+    vector_store=Chroma.from_documents(document_chunks, embedding=embeddings,persist_directory='./vector_data')
     vector_store.persist()
     return vector_store
  
  
-def no_ingest_docs():
-    persist_directory = './vector_data_openAI'
-    embeddings = OpenAIEmbeddings()
-    vector_store = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
-    return vector_store
+
    
  
 def get_context_retriever_chain(vector_store):
     llm = ChatOpenAI(temperature=0.7, model_name='gpt-3.5-turbo')
-    # memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     prompt = ChatPromptTemplate.from_messages([
       MessagesPlaceholder(variable_name="chat_history"),
       ("user", "{input}"),
@@ -54,7 +63,7 @@ def get_context_retriever_chain(vector_store):
 def get_conversational_rag_chain(retriever_chain):  
     llm = ChatOpenAI()  
     prompt = ChatPromptTemplate.from_messages([
-      ("system", "You are a Chat Bot used to answer users based on the information you have in vector db about Mongo DB and you will correctly answer the users based on the knowledge base you have without any hallucinations and assumptions.Now, Answer the user's questions based on the below context:\n\n{context}"),
+      ("system", "As a Chat Bot specialized in Mongo DB and server incident root cause analysis, You are designed to provide accurate responses to user queries regarding Mongo DB and server incident root cause analysis using information stored in your knowledge base. Whenever you don't know answer, you will polietly ask user to add that data url into your knowledge base. Your responses will always free from hallucinations and assumptions, focusing solely on reliable and precise answers related to Mongo DB and server troubleshooting. Now, Answer the user's questions based on the below context:\n\n{context}"),
       MessagesPlaceholder(variable_name="chat_history"),
       ("user", "{input}"),
     ])  
@@ -72,35 +81,31 @@ def get_response(user_input):
     return response['answer']
  
 # app config
-st.set_page_config(page_title="RCA bot ")
-st.title("RCA BOT - TEST 2")
+st.set_page_config(page_title="Mongo Test App")
+st.title("Mongo Bot")
+
  
 # sidebar
 with st.sidebar:
     st.header("Knowledge base")
+    st.write("Add a URL if you wish to feed new data into the knowledge base. The bot will use this data to answer questions.")
     website_url = st.text_input("URL")
  
     if website_url is None or website_url == "":
         st.info("Add a URL to Process New Data")
  
     if st.button("Process New Data"):
-        # session state
-        if "chat_history" not in st.session_state:
-            st.session_state.chat_history = [
-                AIMessage(content="Hello, How can I help you?"),
-            ]
-        if "vector_store" not in st.session_state:
-            st.session_state.vector_store = get_vectorstore_from_url(website_url)        
+        with st.spinner("Processing..."):
+            st.session_state.vector_store = get_vectorstore_from_url(website_url)              
+            st.success("Processed Successfully!")         
  
-if st.button("Use existing Data"):
     # session state
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = [
-            AIMessage(content="Hello, How can I help you?"),
-        ]
-    if "vector_store" not in st.session_state:
-        st.session_state.vector_store = no_ingest_docs()    
- 
+if "chat_history" not in st.session_state:
+    st.session_state.vector_store = no_ingest_docs()
+    st.session_state.chat_history = [
+        AIMessage(content="Hello! How can I assist you today?"),
+    ]
+
 # user input
 user_query = st.chat_input("Type your message here...")
 if user_query is not None and user_query != "":
